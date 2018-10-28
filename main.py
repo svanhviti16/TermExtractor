0343c52805980c5709eb5aa5e370aef69c102620
Svanhvit <svanhviti16@ru.is>
2018-10-28 22:23:37 +0000

    prófa RAKE

---
+++ main.py
@@ -0,0 +1,20 @@
+from nltk import FreqDist
+
+
+
+# FREQUENCY DISTRIBUTION
+fd = FreqDist()
+
+# fyrst: landbúnaðarlög
+# þarf lemmaða skrá
+
+# svo: gullstaðall
+
+# nota lemmuna fyrir freqdist
+
+
+# STOPWORDS
+
+# LONG WORDS
+
+# TERMBASE LOOKUP
--- termbase_extractor.py
+++ termbase_extractor.py
@@ -1,16 +1,39 @@
+# Retrieves Icelandic terms from .tbx files from Íðorðabankinn
+# and saves them to corresponding files
+# Accepts a directory path as a single argument
+# -------------------------
+
 import xml.etree.ElementTree as ET
+import re, os, sys
 
-termfile = open("termbase/uppl.tbx")
+try:
+    # getting path
+    dir_path=sys.argv[1]
+except:
+    print('Please provide a directory path')
 
-tree = ET.parse(termfile)
-root = tree.getroot()
+files = os.listdir(dir_path)
+file_list = [] 
 
-langset_elems = root.findall(".//*langSet")
+for f in files:
+    
+    file_path =  os.path.join(dir_path, f)
+    file_path_str = os.path.basename(file_path)
 
-output = open("termbase/output.txt", 'a')
+    file_prefix = file_path_str.replace(".tbx", "")
+    if file_path.endswith('.tbx'):
+        print(file_path)
+        tree = ET.parse(file_path)
+        root = tree.getroot()
 
-for element in langset_elems:
-    if element.attrib['{http://www.w3.org/XML/1998/namespace}lang'] == "is":
-        terms = element.findall("./*term")
-        for t in terms:
-            output.write(t.text + "\n")
+        # need to first select the language tag to get Icelandic terms
+        langset_elems = root.findall(".//*langSet")
+
+        output = open("termbase/" + file_prefix + ".txt", 'a')
+
+        for element in langset_elems:
+            # finding all terms with the "is" attribute
+            if element.attrib['{http://www.w3.org/XML/1998/namespace}lang'] == "is":
+                terms = element.findall("./*term")
+                for t in terms:
+                    output.write(t.text + "\n")
