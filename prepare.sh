4c995a78422d2a2d4ce7b0f01086cfc95b3bdea6
Svanhvit <svanhviti16@ru.is>
2018-11-01 13:04:21 +0000

    rebase

--- prepare.sh
+++ prepare.sh
@@ -4,16 +4,16 @@
 # tokenizes it, PoS tags it and lemmatizes it using IceNLP.
 
 # tokenizing the input
-java -classpath IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunTokenizer -i $1 -o output/tokenized.txt
+java -classpath ../IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunTokenizer -i $1 -o output/tokenized.txt
 
 # tagging using IceTagger
-java -classpath IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTagger -i output/tokenized.txt -o output/tagged.txt
+java -classpath ../IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTagger -i output/tokenized.txt -o output/tagged.txt
 
 # extracting first two columns (to avoid UNKNOWN tag)
 awk '{print $1 " " $2}' output/tagged.txt > output/tagged_clean.txt
 
 # lemmatizing using Lemmald
-java -classpath IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunLemmald -i output/tagged_clean.txt -o output/lemmatized.txt
+java -classpath ../IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunLemmald -i output/tagged_clean.txt -o output/lemmatized.txt
 
 # reordering to fit Gullstaðall format
 awk '{print $1 " " $3 " " $2}' output/lemmatized.txt > output/lemmatized_clean.txt
@@ -21,6 +21,6 @@ awk '{print $1 " " $3 " " $2}' output/lemmatized.txt > output/lemmatized_clean.t
 # PoS tagging the original file sentence by sentence using IceTagger
 # 2 2 stands for input with one sentence per line and output with one sentence per line
 # TODO: try with raw text
-java -classpath IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTagger -i $1 -o output/tagged_for_parsing.txt LINE_FORMAT=2 OUTPUT_FORMAT=2 
+java -classpath ../IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTagger -i $1 -o output/tagged_for_parsing.txt LINE_FORMAT=2 OUTPUT_FORMAT=2 
 # TODO: remove empty lines
-java -classpath IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParser -i  output/tagged_for_parsing.txt -o output/parsed.txt -f -l
+java -classpath ../IceNLP/dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParser -i  output/tagged_for_parsing.txt -o output/parsed.txt -f -l
